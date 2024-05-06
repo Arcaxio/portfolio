b4fb8ed6eb5c793f252ce75c447e1d4e3d063480
Arcaxio <jasperkueh@gmail.com>
2024-05-06 18:34:46 +0800

    Second Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlights";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <HeroHighlight>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        >
+          Arcaxio&apos;s Portfolio <br></br>
+          <Highlight className="text-black dark:text-white">
+            Just testing things out
+          </Highlight>
+        </motion.h1>
+      </HeroHighlight>
+      <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          Backgrounds
+        </p>
+      </div>
+    </main>
+  );
+}
