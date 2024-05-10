c18fca53dfb72dd257a37bb5b0f67c96e3635187
Arcaxio <jasperkueh@gmail.com>
2024-05-11 03:35:06 +0800

    Added Metadata and Images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,19 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Arcaxio",
-  description: "Arcaxio's Portfolio and Playground",
+  openGraph: {
+    title: 'Arcaxio Portfolio',
+    description: 'Arcaxio\'s Portfolio and Playground',
+    url: 'https://www.arcaxio.com/',
+    siteName: 'arcaxio.com',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    creator: '@arcaxio',
+    creatorId: '1269157663469793280',
+  },
 };
 
 export default function RootLayout({
