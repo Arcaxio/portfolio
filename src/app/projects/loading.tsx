c5d9154d0d7909b4ae1e26dd30ca1bf76b0a7a97
Arcaxio <jasperkueh@gmail.com>
2024-06-01 16:14:07 +0800

    Loading Color changes

--- src/app/projects/loading.tsx
+++ src/app/projects/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="h-dvh w-dvw relative flex flex-col items-center justify-center">
+        <div className="h-dvh w-dvw relative flex flex-col items-center justify-center bg-base-300 text-primary">
             <span className="loading loading-spinner loading-lg"></span>
         </div>
     )
